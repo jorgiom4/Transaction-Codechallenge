41091313e1d70ca289470d8d2c6bdd18933fedce
jorgiom4 <gorkalqy@hotmail.com>
2020-02-05 10:55:52 +0100

    Primer commit de proyecto.

---
+++ src/main/java/com/spring/service/transactioncodechallenge/TransactionCodechallengeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.service.transactioncodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionCodechallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionCodechallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/service/transactioncodechallenge/TransactionCodechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.service.transactioncodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionCodechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
