9e17be5bed57024441979e20e05828ff8c933d55
jorgiom4 <gorkalqy@hotmail.com>
2020-02-06 08:49:47 +0100

    Implementamos el test unitario restantes para validación de las excepciones que debe de lanzar la validación de los parámetros de entrada que vienen al dar de alta una nueva transacción.

--- src/test/java/com/spring/service/transactioncodechallenge/validateTransactionRequestBodyParamsTest.java
+++ src/test/java/com/spring/service/transactioncodechallenge/validateTransactionRequestBodyParamsTest.java
@@ -1,5 +1,6 @@
 package com.spring.service.transactioncodechallenge;
 
+import com.spring.service.transactioncodechallenge.exceptions.AccountBalanceCeroException;
 import com.spring.service.transactioncodechallenge.exceptions.BadParamsNewTransactionException;
 import com.spring.service.transactioncodechallenge.model.dto.TransactionEntityRequest;
 import com.spring.service.transactioncodechallenge.utils.ValidationRequestBodyParam;
@@ -11,8 +12,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+/**
+ * Test unitarios para la validación de los parámetros de entrada de alta nueva transación
+ */
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class validateTransactionRequestBodyParamsTest {
@@ -36,7 +38,10 @@ public class validateTransactionRequestBodyParamsTest {
         validate = new ValidationRequestBodyParam();
     }
 
-    // Validamos que se lanza la excepcion a falta del parámetro account_iban
+    /*
+    Validamos que se lanza la excepcion a falta del parámetro account_iban.
+    La excepción a lanzar es BadParamsNewTransactionException
+    */
     @Test()
     public void validateRequestBodyParamsWithNoAccountIbanParameter(){
         request.setAccount_iban("");
@@ -44,13 +49,56 @@ public class validateTransactionRequestBodyParamsTest {
         validate.validateTransactionRequestBodyParams(request);
     }
 
-    // Validamos que se lanza la excepcion a falta del parámetro account_iban
+    /*
+    Validamos que se lanza la excepcion a falta del parámetro amount
+    La excepción a lanzar es BadParamsNewTransactionException
+    */
     @Test()
     public void validateRequestBodyParamsWithNoAmountParameter(){
         request.setAccount_iban("ES1234565678865432323");
         request.setAmount(0.0);
 
         thrown.expect(BadParamsNewTransactionException.class);
+        validate.validateTransactionRequestBodyParams(request);
+
+    }
+    /*
+    Validamos que se lanza la excepcion a falta de los parámetros iban y amount
+    La excepción a la lanzar es BadParamsNewTransactionException
+    */
+    @Test()
+    public void validateRequestBodyParamsWithNoIbanAndAmountParameter(){
+        request.setAccount_iban("");
+        request.setAmount(0.0);
+
+        thrown.expect(BadParamsNewTransactionException.class);
+        validate.validateTransactionRequestBodyParams(request);
+
+    }
+
+    /*
+    Validamos que se lanza la excepción cuando el balance de la transacción es = 0
+    La excepción a lanzar es AccountBalanceCeroException
+    */
+    @Test
+    public void validateRequestBodyParamsWithBalanceEqualsCero(){
+        request.setAmount(23.20);
+        request.setFee(23.20);
+
+        thrown.expect(AccountBalanceCeroException.class);
+        validate.validateTransactionRequestBodyParams(request);
+    }
+
+    /*
+    Validamos que se lanza la excepción cuando el balance de la transacción es < 0
+    La excepción a lanzar es AccountBalanceCeroException
+    */
+    @Test
+    public void validateRequestBodyParamsWithBalanceLessThanCero(){
+        request.setAmount(23.20);
+        request.setFee(30.20);
+
+        thrown.expect(AccountBalanceCeroException.class);
         validate.validateTransactionRequestBodyParams(request);
 
     }
